295a938ca59eb2ace865b531f19de10cc1a94ed4
109taha <109taha@gmail.com>
2024-01-12 06:11:20 +0500

    add changes

--- controller/auth.js
+++ controller/auth.js
@@ -1,9 +1,13 @@
 const AdminModel = require("../models/admin");
 const UserModel = require("../models/user");
+const AdminJoi = 
 
 module.exports.AdminRegister = async (req, res) => {
   try {
     const body = req.body;
+    const result = userSchema.validate(req.body, {
+        abortEarly: false,
+      });
   } catch (error) {
     console.error(error);
     res.status(500).send({ success: false, message: error });
