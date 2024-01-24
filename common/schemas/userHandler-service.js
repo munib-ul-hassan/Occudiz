d3679cf422fc75f4650bf35b5e1a37383a1eb240
109taha <109taha@gmail.com>
2024-01-25 03:53:05 +0500

    userhandler

--- common/schemas/userHandler-service.js
+++ common/schemas/userHandler-service.js
@@ -1,17 +1,29 @@
 const registerSchema = {
-    type: "object",
-    properties: {
-        name: { type: "string" },
-        email: { type: "string", format: "email" },
-        password: {
-            type: "string",
-            // minLength: 8,
-            // pattern: PASSWORD_REGEX,
-        },
-        roleId: { type: "number" },
-        type: { type: "string" }
+  type: "object",
+  properties: {
+    name: { type: "string" },
+    email: { type: "string", format: "email" },
+    hashedPassword: {
+      type: "string", // minLength: 8, pattern: PASSWORD_REGEX
     },
-    required: ["email", "password"],
+    phoneNumber: { type: "number" },
+    role: { type: "number" },
+    token: { type: "string" },
+    type: { type: "string" },
+    idCard: { type: "string" },
+    businessRegisterNum: { type: "string" },
+    active: { type: "boolean" },
+    fcmToken: { type: "string" },
+  },
+  required: [
+    "email",
+    "password",
+    "name",
+    "phoneNumber",
+    "role",
+    "type",
+    "active",
+  ],
 };
 
 module.exports = { registerSchema };
