ab35c590c7d1f2c55d5dfa25c7308a6b7dbd5081
Sami Khan <83097078+SamiKhan979@users.noreply.github.com>
2024-01-27 23:05:12 +0500

    Create multer.js

---
+++ common/middleware/multer.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const multer = require("multer");
+
+const MAX_FILE_SIZE = 5 * 1024 * 1024; // 20 MB (in bytes)
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(process.cwd(), "/src/tmp/images"));
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname.replace(/ /g, '-')}`);
+    }
+});
+
+const imageFilter = (req, file, cb) => {
+    if (file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
+        cb(null, true);
+    } else {
+        cb(new Error("Only image files are allowed!"));
+    }
+};
+
+const upload = multer({
+    storage,
+    fileFilter: imageFilter,
+    limits: {
+        fileSize: MAX_FILE_SIZE,
+    }
+});
+
+module.exports = upload;
